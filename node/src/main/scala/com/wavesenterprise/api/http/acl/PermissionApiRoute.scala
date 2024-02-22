de1a2b213ef991b765ade73dd2ecef3219574af6
egnatovskiy <egnatovskiy@web3tech.ru>
2024-02-22 18:10:05 +0700

    WE-8518-fix-incorect-timestamp

--- node/src/main/scala/com/wavesenterprise/api/http/acl/PermissionApiRoute.scala
+++ node/src/main/scala/com/wavesenterprise/api/http/acl/PermissionApiRoute.scala
@@ -3,6 +3,7 @@ package com.wavesenterprise.api.http.acl
 import akka.http.scaladsl.server.Route
 import com.wavesenterprise.account.Address
 import com.wavesenterprise.api.ValidInt._
+import com.wavesenterprise.api.ValidLong._
 import com.wavesenterprise.api.http.ApiRoute
 import com.wavesenterprise.api.http.service.PermissionApiService
 import com.wavesenterprise.settings.ApiSettings
@@ -45,10 +46,12 @@ class PermissionApiRoute(val settings: ApiSettings,
     *
     * Get all active permissions for address at timestamp
     **/
-  def forAddressAtTimestamp: Route = (get & path(Segment / "at" / LongNumber)) { (addressStr, timestamp) =>
-    withExecutionContext(scheduler) {
-      complete {
-        permissionApiService.forAddressAtTimestamp(addressStr, timestamp)
+  def forAddressAtTimestamp: Route = (get & path(Segment / "at" / Segment)) { (addressStr, timestampStr) =>
+    PositiveLong(timestampStr).processRoute { timestamp =>
+      withExecutionContext(scheduler) {
+        complete {
+          permissionApiService.forAddressAtTimestamp(addressStr, timestamp)
+        }
       }
     }
   }
